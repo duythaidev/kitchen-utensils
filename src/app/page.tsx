7c5ea90b907abddae7d80c4cc500dbe937688600
duythaidev <nguyenduythai17092005@gmail.com>
2025-05-13 00:50:12 +0700

    first commit with next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,77 @@
-import Image from "next/image";
+'use client'
+import React, { useRef, useState } from 'react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <main className="dark:bg-gray-800 bg-white relative overflow-hidden h-screen">
+        <header className="h-24 sm:h-32 flex items-center z-30 w-full">
+          <div className="container mx-auto px-6 flex items-center justify-between">
+            <div className="uppercase text-gray-800 dark:text-white font-black text-3xl">
+              Watch.ME
+            </div>
+            <div className="flex items-center">
+              <nav className="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+                <a href="#" className="py-2 px-6 flex">
+                  Home
+                </a>
+                <a href="#" className="py-2 px-6 flex">
+                  Watch
+                </a>
+                <a href="#" className="py-2 px-6 flex">
+                  Product
+                </a>
+                <a href="#" className="py-2 px-6 flex">
+                  Contact
+                </a>
+                <a href="#" className="py-2 px-6 flex">
+                  Carrer
+                </a>
+              </nav>
+              <button className="lg:hidden flex flex-col ml-4">
+                <span className="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                </span>
+                <span className="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                </span>
+                <span className="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                </span>
+              </button>
+            </div>
+          </div>
+        </header>
+        <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
+          <div className="container mx-auto px-6 flex relative py-16">
+            <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+              <span className="w-20 h-2 bg-gray-800 dark:bg-white mb-12">
+              </span>
+              <h1
+                className="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
+                Be on
+                <span className="text-5xl sm:text-7xl">
+                  Time
+                </span>
+              </h1>
+              <p className="text-sm sm:text-base text-gray-700 dark:text-white">
+                Dimension of reality that makes change possible and understandable. An indefinite and homogeneous
+                environment in which natural events and human existence take place.
+              </p>
+              <div className="flex mt-8">
+                <a href="#"
+                  className="uppercase py-2 px-4 rounded-lg bg-pink-500 border-2 border-transparent text-white text-md mr-4 hover:bg-pink-400">
+                  Get started
+                </a>
+                <a href="#"
+                  className="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-pink-500 dark:text-white hover:bg-pink-500 hover:text-white text-md">
+                  Read more
+                </a>
+              </div>
+            </div>
+            <div className="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
+              <img src="https://www.tailwind-kit.com/images/object/10.png" className="max-w-xs md:max-w-sm m-auto" />
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
