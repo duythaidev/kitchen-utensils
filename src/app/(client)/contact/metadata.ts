2cd23bc9d5e96a48d8fba6f4056340f667b3eaa6
duythaidev <nguyenduythai17092005@gmail.com>
2025-07-08 02:06:01 +0700

    feat: fetch statistics data to admin dashboard, add user order history, add admin change order status

--- src/app/(client)/contact/metadata.ts
+++ src/app/(client)/contact/metadata.ts
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-
 export const metadata: Metadata = {
     title: 'Contact Us - Kitchen Utensils',
     description: 'Get in touch with our customer support team for any questions or assistance',
